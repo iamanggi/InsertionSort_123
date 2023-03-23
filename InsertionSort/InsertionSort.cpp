c87293456c8ad64ffc9717ef9e007b2de0283726
iamanggi <anggipuspita452@gmail.com>
2023-03-23 15:37:33 +0700

    menampilkan lopping j

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -72,4 +72,10 @@ void display()
 	cout << "total element movement " << n - 1 << endl;		// cout movement element
 	cout << "\n=====================" << endl;				// output ke layar
 	cout << "element array yang telah tersusun" << endl;	//output ke layar
+	cout << "\n=====================" << endl;				// output ke layar
+
+	for (int j = 0; j < n; j++)
+	{														// looping j dimulai dari 0 hingga n-1 
+		cout << arr[j] << endl;								// output ke layar
+	}
 }
